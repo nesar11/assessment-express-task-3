137c7eb9a83cbe91cbcff7869baeef5ef3c8f505
nesar11 <nesar.uddin100@gmail.com>
2023-04-15 20:02:09 +0600

    file upload

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,32 +1,29 @@
 const express = require('express');
 const app = express();
 const productRoutes = express.Router();
+// const extractFile = require('../middleware/file')
 
-const Product = require('../model/Product')
 
-productRoutes.post('/add', async (req, res)=>{
-    const newProduct = new Product(req.body);
-    try{
-        const savedProduct = await newProduct.save()
-        req.status(200).json(savedProduct);
-        console.log(savedProduct)
-        
-    } catch (err){
-        res.status(500).json(err)
-    }
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination : function (req, file, cb){
+    return cb(null, "uploads")
+  },
+  filename: function (req, file, cb) {
+    return cb( null, `${Date.now()}-${file.originalname}`)
     
+  }
+})
+const upload = multer({storage});
+
+productRoutes.post('/upload', upload.single('ProductImage'),(req, res)=>{
+  console.log(req.body)
+  console.log(req.file)
+  return res.redirect('/')
 
 })
 
 
-productRoutes.get("/", async (req, res) => {
-    try {
-      const products = await Product.find({ });
-      res.json(products);
-      console.log(products);
-    } catch (err) {
-      console.log(err);
-    }
-  });
 
 module.exports =  productRoutes
--- server.js
+++ server.js
@@ -6,15 +6,29 @@ cors = require("cors"),
 mongoose = require("mongoose"),
 config = require('./config/DB');
 const productRoutes = require('./Routes/productRoutes')
+
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB).then(
     ()=>{console.log('Database is connect')},
     err=>{ console.log('Can not connect to the databse' +err)}
 );
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
 app.use("/products", productRoutes)
 
 const port = process.env.PORT || 5000;
